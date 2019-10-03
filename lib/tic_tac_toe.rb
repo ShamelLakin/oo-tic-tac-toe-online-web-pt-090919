db919eea13c8fa1588a56e160634b2b382a698b0
Shamel Lakin <christlife4u@gmail.com>
2019-10-03 15:41:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,10 +75,10 @@ def turn
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-       win_combo
+      return win_combo
     end
   }
-   false
+   return false
 end
 
  
