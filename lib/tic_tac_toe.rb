8f468a205860c74013b4265ac63f92456fa75007
Shamel Lakin <christlife4u@gmail.com>
2019-10-03 15:23:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,11 +53,11 @@ class TicTacToe
     "O"
   end
   
-  def turn 
-    puts "Enter a number 1 - 9:"
-    user_input.strip
-    index = input_to_index(input)
-  end 
+  # def turn 
+  #   puts "Enter a number 1 - 9:"
+  #   user_input.strip
+  #   index = input_to_index(input)
+  # end 
  
  
 end 
